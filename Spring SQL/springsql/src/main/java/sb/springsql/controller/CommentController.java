46bc1ac15caa95b08ed8360667f332cc7b915f2e
Jeff Catulay <catulay.programming.acc@gmail.com>
2023-09-09 09:24:05 +0800

    spring sql v3

--- Spring SQL/springsql/src/main/java/sb/springsql/controller/CommentController.java
+++ Spring SQL/springsql/src/main/java/sb/springsql/controller/CommentController.java
@@ -8,8 +8,10 @@ import sb.springsql.entity.Comment;
 import sb.springsql.payload.CommentDto;
 import sb.springsql.service.CommentService;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api/post/")
 public class CommentController {
     @Autowired
     private CommentService service;
@@ -18,5 +20,36 @@ public class CommentController {
     public ResponseEntity<CommentDto> createComment(@PathVariable("postId") long postId,
                                                     @RequestBody CommentDto commentDto){
         return new ResponseEntity<>(service.createComment(postId, commentDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("{postId}/comments")
+    public List<CommentDto> getCommentsByPostId(@PathVariable("postId") long postId){
+        return service.getCommentByPostId(postId);
+    }
+
+    @GetMapping("{postId}/comments/{id}")
+    public ResponseEntity<CommentDto> getCommentsById(@PathVariable("postId") long postId,
+    @PathVariable("id") long commentId){
+        CommentDto commentDto = service.getCommentById(postId, commentId);
+        return new ResponseEntity<>(commentDto, HttpStatus.OK);
+    }
+
+    @PutMapping("{postId}/comments/{id}")
+    public ResponseEntity<CommentDto> updateComment(
+            @PathVariable("postId") long postId,
+            @PathVariable("commentId") long commentId,
+            @RequestBody CommentDto commentDto){
+        CommentDto updatedComment = service.updateComment(postId, commentId, commentDto);
+        return new ResponseEntity<>(updatedComment, HttpStatus.OK);
+    }
+
+    @DeleteMapping("{postId}/comments/{id}")
+    public ResponseEntity<String> deleteComment(
+            @PathVariable("postId") long postId,
+            @PathVariable("commentId") long commentId
+            )
+    {
+        service.deleteComment(postId, commentId);
+        return new ResponseEntity<>("Deleted", HttpStatus.OK);
     }
 }
